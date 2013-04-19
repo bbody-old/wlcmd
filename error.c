2da831b509e5305cecacf45d7bb750e47a0ac9e5
bbody <bbody.project@gmail.com>
2013-04-19 22:07:54 +1000

    Added open source licence.

--- error.c
+++ error.c
@@ -1,3 +1,18 @@
+/*This file is part of Wlcmd.
+
+Wlcmd is free software: you can redistribute it and/or modi
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Wlcmd is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Wlcmd.  If not, see <http://www.gnu.org/licenses/>.*/
+
 #include <stdio.h>
 #include "error.h"
 #include "wlcmd.h"
